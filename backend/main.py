23af3ff13fe1c4b7cec20be3755839e3521f6cf4
ministerangebenit-dotcom <ministerangebenit@gmail.com>
2026-06-28 15:02:58 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,20 @@
+from fastapi import FastAPI
 
+app = FastAPI(
+    title="Dalil API",
+    version="1.0.0"
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to the Dalil API!"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
